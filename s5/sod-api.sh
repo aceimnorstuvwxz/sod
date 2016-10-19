245584610cebef9fc081fd6a7a766c3e4a481026
dgkae <dgkae.cbf@gmail.com>
2016-10-19 17:10:22 +0800

    userdict union test

--- s5/sod-api.sh
+++ s5/sod-api.sh
@@ -11,6 +11,17 @@ rm -rf /home/dgk/sodwp/$wavid
 mkdir /home/dgk/sodwp/$wavid
 cp "$wavfn" /home/dgk/sodwp/$wavid/T0_1.wav
 cp /home/dgk/sodwp/T0_1.wav.trn /home/dgk/sodwp/$wavid/T0_1.wav.trn
-./sod.sh $wavid > $logf
-cat ./exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/result.ar
+time ./sod.sh $wavid > $logf
+pa=` pwd `
+cd /home/dgk/dgkfiles/kaldi-lattice-userdict
+time ./routine_lka.py $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/lat.txt $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/lat2.txt
+source /home/dgk/enable_kaldi_tools.sh
+time lattice-1best --acoustic-scale=0.1 ark:$pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/lat2.txt ark,t:$pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/1best.txt
+time ./routine_tpm.py $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/1best.txt $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/result.txt
+
+
+cat $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/result.txt
+echo " "
+cat $pa/exp/tri4b_dnn_mpe/decode_${wavid}_word_it3/result.ar 
+
 
